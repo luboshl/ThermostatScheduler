7a53bed14a9220f7a4a1d31e67371e64ce83c0e6
Lubos Hladik <luboshl@users.noreply.github.com>
2021-10-31 16:32:45 +0100

    Forward default page to scheduled event list.

--- src/ThermostatScheduler.WebApp/Pages/Default/DefaultViewModel.cs
+++ src/ThermostatScheduler.WebApp/Pages/Default/DefaultViewModel.cs
@@ -1,10 +1,18 @@
-﻿namespace ThermostatScheduler.WebApp.Pages.Default
+﻿using System.Threading.Tasks;
+
+namespace ThermostatScheduler.WebApp.Pages.Default
 {
     public class DefaultViewModel : MasterPageViewModel
     {
         public DefaultViewModel(IDependencies dependencies)
             : base(dependencies)
         {
+        }
+
+        public override Task PreRender()
+        {
+            Context.RedirectToRoute(Routes.ScheduledEvents.ScheduledEventList);
+            return Task.CompletedTask;
         }
     }
 }
